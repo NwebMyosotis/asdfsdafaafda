324d88da3107cbf0bc536f75d81e8026a29c32de
NwebMyosotis <dynamickid@naver.com>
2023-07-11 16:17:17 +0900

    warn 문제 해결

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import useProducts from "../hooks/use-producs";
 
 export default function Products() {
